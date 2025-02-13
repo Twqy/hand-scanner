cbed2096f6e2b9f6c8ee0cc543e77ed616e1252c
Ryan Tran <78041808+Twqy@users.noreply.github.com>
2025-02-13 02:10:15 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
+#real-time hand tracking and inference using mediapipe library and opencv
 
+import cv2
+import mediapipe as mp
+
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mphands = mp.solutions.hands
+
+cap = cv2.VideoCapture(0) 
+hands = mphands.Hands()
+while True:
+    data, image = cap.read()
+    # Flip the image
+    image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+    # Storing the results
+    results = hands.process(image)
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            mp_drawing.draw_landmarks(
+                image, hand_landmarks, mphands.HAND_CONNECTIONS)
+    cv2.imshow('Handtracker', image)
+    
+    # Exit the loop when 'ESC' key is pressed
+    if cv2.waitKey(1) & 0xFF == 27:
+        break
+
+# Release the video capture object and close all OpenCV windows
+cap.release()
+cv2.destroyAllWindows()
